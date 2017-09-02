2fb27aa2205592f86fc15e43634406bbd4bee832
rajdeepDS <rajdeep@doublespring.com>
2017-09-03 02:59:26 +0530

    output management -> Development

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
-   // Add the image to our existing div.
-   var myIcon = new Image();
-   myIcon.src = Icon;
-
-   element.appendChild(myIcon);
-   console.log(Data);
   return element;
 }
 
